c77d288df404d1426f7cb7274f6c4d4195018f31
mostafa elbannan <mostafaelbannan1@gmail.com>
2022-01-11 10:09:34 -0800

    added tailwind to project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import { vuetify } from "./plugins/vuetify";
-import router from "./router";
-import i18n from "./plugins/i18n";
-import { store, key } from "./store/index";
+import { createApp } from "vue"
+import App from "./App.vue"
+import { vuetify } from "./plugins/vuetify"
+import router from "./router"
+import i18n from "./plugins/i18n"
+import { store, key } from "./store/index"
+import "./styles/tailwind.scss"
 
-createApp(App).use(store, key).use(router).use(vuetify).use(i18n).mount("#app");
+createApp(App)
+    .use(store, key)
+    .use(router)
+    .use(vuetify)
+    .use(i18n)
+    .mount("#app");
