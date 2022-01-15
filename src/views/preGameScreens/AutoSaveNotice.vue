e7516842ef330cfe82b545c92000ba9518edccfd
mostafa elbannan <mostafaelbannan1@gmail.com>
2022-01-14 21:14:44 -0800

    fixed a bug

--- src/views/preGameScreens/AutoSaveNotice.vue
+++ src/views/preGameScreens/AutoSaveNotice.vue
@@ -24,17 +24,11 @@ import { onUnmounted } from 'vue'
 import { useRouter } from 'vue-router'
 
 import autoSaveIcon from '@/components/misc/autoSaveIcon.vue'
-import { DEVICE_IS_COMPATIBLE_LOCALSTORAGE_KEY } from './DeviceCompatiblity.vue'
 
 const router = useRouter()
 const milliseconds = 5_000
 function onNext() {
-    const deviceCompatiblityTestPassed = window.localStorage.getItem(DEVICE_IS_COMPATIBLE_LOCALSTORAGE_KEY)
-    if (!deviceCompatiblityTestPassed) {
-        router.push("/device-compatiblity")
-    } else {
-        router.push("/main-menu")
-    }
+    router.push("/main-menu")
 }
 const timeoutId = window.setTimeout(onNext, milliseconds)
 window.addEventListener('keydown', onNext)
